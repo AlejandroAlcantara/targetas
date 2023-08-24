c68f33efb64613bcdca21f707766a1b973dcf376
marizol <alejandroalcantaraalcantara@gmail.com>
2023-08-24 01:45:22 -0600

    letras

--- src/componentes/Footer/index.jsx
+++ src/componentes/Footer/index.jsx
@@ -1,6 +1,6 @@
-import footerbg from "../../assets/footer.png";
+import "./Footer.css";
 const Footer = ()=>{
-    return <footer className="footer" style={{backgroundImage:`url(${footerbg})`}}>
+    return <footer className="footer" style={{backgroundImage:"url(/img/footer.png)"}}>
         <div className="redes">
         <a href='https://www.facebook.com/alezz.alcantara'> 
             <img src="/img/facebook.png" alt="Facebook"/>
